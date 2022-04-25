df8e682343b453a37d9f4c2da6d53f759195a8a1
Christina_Graham <cgraham74@gmail.com>
2022-04-24 20:29:51 -0500

    Created a Dynamic Array class

---
+++ src/Array.java
@@ -0,0 +1,58 @@
+public class Array {
+
+    private int[] items;
+    private int count;
+
+    public Array(int length){
+        items = new int[length];
+    }
+
+    public void insert (int item){
+        // If the array is full, resize it
+        if (items.length == count){
+
+            //Create a new array (twice the size)
+            int[] newItems = new int[count * 2];
+
+            //Copy all the existing items
+            for (int i = 0; i < count; i++){
+                newItems[i] = items[i];
+                //Set "items" to this new array
+            }
+            items = newItems;
+        }
+        // Add the new item at the end
+        items[count++] = item;
+    }
+
+    public void removeAt(int index){
+        //Validate the index
+        if (index < 0 || index >= count){
+            throw new IllegalArgumentException();
+        }
+        for (int i = index; i < count; i++){
+            items[i] = items[i + 1];
+        }
+        count--;
+        //Shift items to the left to fill the hole
+    }
+
+    public int indexOf(int item){
+        // if we find it, return indext
+        // Otherwise, return -1
+        for (int i = 0; i < count; i++){
+            if(items[i] == item){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void print(){
+        for (int i = 0; i < count; i++){
+            System.out.println(items[i]);
+        }
+    }
+
+
+}
